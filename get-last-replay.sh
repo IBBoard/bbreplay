3a3ef2cfe12619acf141847dfe3e2d08c1d08cdf
IBBoard <dev@ibboard.co.uk>
2021-04-07 20:03:20 +0100

    Quick script to grab a log and replay
    It probably has limits, but it works for now!

---
+++ get-last-replay.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+bb_base=~/.local/share/Steam/steamapps/compatdata/216890/pfx/drive_c/
+bb_log=${bb_base}users/steamuser/My\ Documents/BloodBowlChaos/BB_Chaos000.log
+
+# NOTE: Original and Legendary editions will have different IDs and slightly different log names
+last_replay=$(grep -B1 "Set match version" "${bb_log}" | head -n1 | cut -f2- -d/ | sed 's/\r//')
+last_replay_file=${last_replay##*/}
+mkdir -p temp/
+cp "${bb_log}" "temp/${last_replay_file/.db}.log"
+cp "${bb_base}${last_replay}" "temp/${last_replay_file}"
