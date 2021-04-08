c99f22feae380992d3efa4a8f3982b7f326cf688
IBBoard <dev@ibboard.co.uk>
2021-04-08 14:36:30 +0100

    Update "get replay" script to handle new games
    The old pattern matching only worked when a replay had been watched
    and not when a new game had been played.

--- get-last-replay.sh
+++ get-last-replay.sh
@@ -4,7 +4,7 @@ bb_base=~/.local/share/Steam/steamapps/compatdata/216890/pfx/drive_c/
 bb_log=${bb_base}users/steamuser/My\ Documents/BloodBowlChaos/BB_Chaos000.log
 
 # NOTE: Original and Legendary editions will have different IDs and slightly different log names
-last_replay=$(grep -B1 "Set match version" "${bb_log}" | head -n1 | cut -f2- -d/ | sed 's/\r//')
+last_replay=$(grep "Open database.*Replay.*\.db" "${bb_log}" | tail -n1 | cut -f2- -d/ | sed 's/\r//')
 last_replay_file=${last_replay##*/}
 mkdir -p temp/
 cp "${bb_log}" "temp/${last_replay_file/.db}.log"
