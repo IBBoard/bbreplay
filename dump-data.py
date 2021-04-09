d68a6147c655586ead6a29a4338047f4e44ad002
IBBoard <dev@ibboard.co.uk>
2021-04-09 11:03:10 +0100

    Rename "Summarise" to "dump-data"
    We're not actually summarising anything. Just dumping what we
    know about the replay!

--- dump-data.py
+++ dump-data.py
@@ -8,10 +8,11 @@ from bbreplay.command import NetworkCommand
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process a Blood Bowl replay file.')
+    parser = argparse.ArgumentParser(description='Process a Blood Bowl replay file and dump internal representations')
     parser.add_argument('replay_file', metavar='replay-file', help='the replay database to parse')
     parser.add_argument('log_file', metavar='log-file', help='the replay log to parse')
-    parser.add_argument('--verbose', '-v', action='store_true', help='include verbose messages (including suspected network traffic)')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='include verbose messages (including suspected network traffic)')
     args = parser.parse_args()
 
     print(f"{os.path.basename(args.replay_file)}")
@@ -20,18 +21,23 @@ if __name__ == '__main__':
     print(f"Home: {home_team.name} ({home_team.race})")
     print(f"Away: {away_team.name} ({away_team.race})")
 
-    print("+++ Events")
+    print("\n+++ Events")
 
     for event in replay.events():
-        print(event)
+        # Fudge the output so that we're not dumping the board each time,
+        # because it gets messy and unreadable
+        event_details = event._asdict()
+        if 'board' in event_details:
+            del(event_details['board'])
+        print(f"{type(event).__name__}{event_details}")
 
-    print("+++ Commands")
+    print("\n+++ Commands")
 
     for cmd in replay.get_commands():
         if args.verbose or not cmd.is_verbose:
             print(cmd)
 
-    print("+++ Log entries")
+    print("\n+++ Log entries")
 
     for log in replay.get_log_entries():
         print(log)
