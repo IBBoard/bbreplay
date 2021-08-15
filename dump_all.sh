18fd7332e43a031899b608be0dd7134fbd12dc9a
IBBoard <dev@ibboard.co.uk>
2021-08-15 15:36:56 +0100

    Make dumping replays to text repeatable & parallel

---
+++ dump_all.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Temporarily ignore "child process done" monitoring
+set +m
+pids=()
+for replay in temp/*.db
+do
+    # Background each task and track the PID so that we can wait for it to end
+    python3 dump-data.py --debug $replay ${replay/.db}.log >${replay/.db}.txt 2>&1 &
+    pids[${#pids[@]}]=$!
+done 2>&1 | grep -v "^[\[0-9\]+]"  # Hide the "[n] PID" output
+
+for pid in ${pids[*]}
+do
+    wait $pid
+done
+
+# Restore "child process done" monitoring
+set -m
