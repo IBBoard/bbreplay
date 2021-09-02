34ffc4893d509fd2dfde15262bbd5ee5a5813a1a
IBBoard <dev@ibboard.co.uk>
2021-09-02 16:21:05 +0100

    Log where we're having success in parsing full files
    This is currently shorter than printing the ones that we fail on!

--- dump_all.sh
+++ dump_all.sh
@@ -6,7 +6,7 @@ pids=()
 for replay in temp/*.db
 do
     # Background each task and track the PID so that we can wait for it to end
-    python3 dump-data.py --debug $replay ${replay/.db}.log >${replay/.db}.txt 2>&1 &
+    (python3 dump-data.py --debug $replay ${replay/.db}.log >${replay/.db}.txt 2>&1 && echo "$replay completed successfully") &
     pids[${#pids[@]}]=$!
 done 2>&1 | grep -v "^[\[0-9\]+]"  # Hide the "[n] PID" output
 
